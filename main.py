7dc69ef39687060f2af36ad285f643ad4d1c6e56
Kyle Lehning <lehnink@gmail.com>
2019-09-23 18:45:30 -0700

    Completed assignment with return redirect

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, redirect
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -17,12 +17,23 @@ def get_fact():
     return facts[0].getText()
 
 
+def get_pig_latin(text_string):
+    request_url = "https://hidden-journey-62459.herokuapp.com/piglatinize/"
+    input_text = {"input_text": text_string}
+    response = requests.post(request_url, data=input_text, allow_redirects=False)
+
+    return response.headers.get('location')
+
+
 @app.route('/')
 def home():
-    return "FILL ME!"
+    new_fact = get_fact().strip()
+    link_url = get_pig_latin(new_fact)
+    # body = f"<a href={link_url}>{link_url}</a>"
+    return redirect(link_url)
+    # return Response(response=body)
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6787))
     app.run(host='0.0.0.0', port=port)
-
